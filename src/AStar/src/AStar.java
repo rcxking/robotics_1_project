eecac835d48944d303091a3dfa39ff005cabc9a6
Bryant Pong <rcxking@gmail.com>
2014-11-17 20:15:12 -0500

    AStar works

--- src/AStar/src/AStar.java
+++ src/AStar/src/AStar.java
@@ -7,13 +7,11 @@
  * CSCI-4480
  * 11/10/14
  * 
- * Last Updated: 11/11/14 - 7:44 PM
+ * Last Updated: 11/17/14 - 6:31 PM
  */
 
 // Data Structures:
 import java.util.*; // Needed for Java's Priority Queue and Vector
-
-
 
 public class AStar {
 
@@ -83,11 +81,14 @@ public class AStar {
 			
 			startBoard.add(startRow);
 			endState.add(endRow);
+		
 		} // End for
 		
 		System.out.println("The Starting Board is:");
 		printBoard(startingBoard);
+		System.out.println("startBoard is: ");
 		printBoard(startBoard);
+		System.out.println("endState is: ");
 		printBoard(endState);
 		System.out.println("The Ending Location is at: (" + desX + "," + desY + ")");
 		System.out.println("The starting Manhattan Distance is: " + calculateManhattanDistance(startBoard));
@@ -126,11 +127,11 @@ public class AStar {
 		for(int i = 0; i < state.size(); i++) {
 			for(int j = 0; j < state.get(i).size(); j++) {
 				System.out.print(state.get(i).get(j));
-			}
+			} // End for
 			System.out.println();
-		}
+		} // End for
 		System.out.println();
-	}
+	} // End function printBoard()
 	
 	// This function compares whether two boards are the same:
 	public boolean areBoardsSame(Vector<Vector<String>> board1, Vector<Vector<String>> board2) {
@@ -139,12 +140,108 @@ public class AStar {
 			for(int j = 0; j < board1.get(0).size(); j++) {
 				if(!(board1.get(i).get(j).equalsIgnoreCase(board2.get(i).get(j)))) {
 					return false;
-				}
-			}
-		}
+				} // End if
+			} // End for
+		} // End for
 		return true;
-		
 	} // End function areBoardsSame()
+	
+	// This function checks whether a board has been seen in a list of boards:
+	public boolean hasBoardBeenExplored(Vector<Vector<String>> boardToFind, 
+										Vector<Vector<Vector<String>>> listOfBoards) {
+		
+		for(int i = 0; i < listOfBoards.size(); i++) {
+			if(areBoardsSame(boardToFind, listOfBoards.get(i))) {
+				 return true;
+			} // End if
+		} // End for
+		return false;
+	} // End function hasBoardBeenExplored()
+	
+	/* This function returns a move.  A move can be in any of the 8 adjacent squares:
+	 * 
+	 * The location is specified by a number between 0 and 7.  This number corresponds
+	 * to the following moves:
+	 * 
+	 * 0 1 2
+	 * 3 C 4
+	 * 5 6 7
+	 */
+	public Vector<Vector<String>> makeMove(Vector<Vector<String>> currentBoard, 
+			                               int row, int col, 
+			                               int loc) {
+		
+		// Make a copy of the current board:
+		Vector<Vector<String>> nextMove = new Vector<Vector<String>>();
+		
+		for(int i = 0; i < currentBoard.size(); i++) {
+			
+			Vector<String> temp = new Vector<String>();
+			
+			for(int j = 0; j < currentBoard.get(i).size(); j++) {
+				temp.addElement(currentBoard.get(i).get(j));
+			} // End for
+			
+			nextMove.addElement(temp);
+		} // End for
+			
+		// Make the appropriate move:
+		switch(loc) {
+		
+			// Case 0: Upper-left corner:
+			case 0:
+				nextMove.get(row).set(col, "0");
+				nextMove.get(row-1).set(col - 1, "C");
+				break;
+			// Case 1: Upper-Middle:
+			case 1:
+				nextMove.get(row).set(col, "0");
+				nextMove.get(row-1).set(col, "C");
+				break;
+			// Case 2: Upper-right corner
+			case 2:
+				nextMove.get(row).set(col, "0");
+				nextMove.get(row-1).set(col + 1, "C");
+				break;
+			// Case 3: Left Middle
+			case 3:
+				nextMove.get(row).set(col, "0");
+				nextMove.get(row).set(col - 1, "C");
+				break;
+			// Case 4: Right Middle
+			case 4:
+				nextMove.get(row).set(col, "0");
+				nextMove.get(row).set(col + 1, "C");
+				break;
+			// Case 5: Lower-left corner
+			case 5:
+				nextMove.get(row).set(col, "0");
+				nextMove.get(row + 1).set(col - 1, "C");
+				break;
+			// Case 6: Lower Middle
+			case 6:
+				nextMove.get(row).set(col, "0");
+				nextMove.get(row + 1).set(col, "C");
+				break;
+			// Case 7: Lower-right corner
+			case 7:
+				nextMove.get(row).set(col, "0");
+				nextMove.get(row + 1).set(col + 1, "C");
+				break;
+
+			// We should never reach this state:
+			default:
+				System.err.println("ERROR: INVALID MOVE");
+				System.exit(1);
+				break;
+			
+		} // End switch
+		
+		//System.out.println("nextMove is: ");
+		//printBoard(nextMove);
+		
+		return nextMove;
+	} // End function makeMove()
 	
 	// This function returns a list of all the possible moves in a given state:
 	public Vector<Vector<Vector<String>>> possibleMoves(State currentState) {
@@ -152,9 +249,12 @@ public class AStar {
 		// Vector of possible moves:
 		Vector<Vector<Vector<String>>> moves = new Vector<Vector<Vector<String>>>();
 		
+		// Copy of the current map:
+		Vector<Vector<String>> copyMap = new Vector<Vector<String>>(currentState.getCurrentState());
+		
 		// Find the current X and Y location:
-		int currentXLoc, currentYLoc;
-		currentXLoc = currentYLoc = 0;
+		int row, col;
+		row = col = 0;
 		
 		for(int i = 0; i < currentState.getCurrentState().size(); i++) {
 			for(int j = 0; j < currentState.getCurrentState().get(i).size(); j++) {
@@ -162,25 +262,145 @@ public class AStar {
 				if(currentState.getCurrentState().get(i).get(j).equalsIgnoreCase("C")) {
 					// We found the current location!
 					
-					currentXLoc = j;
-					currentYLoc = i;
+					col = j;
+					row = i;
 					
 					break;
 				} // End if
 			} // End for
 		} // End for
 		
+	
 		// DEBUG ONLY - Print out the value of the current position:
+		System.out.println("The currentLocation is at: (" + col + "," + row + ")");
 		
+		/*
+		// DEBUG ONLY - Print out the copy of the map:
+		copyMap.get(0).set(0, "69");
+		System.out.println("copyMap is: " + copyMap);
+		System.out.println("currentState.getCurrentState is: " + currentState.getCurrentState());
+		*/
 		
+		/*
+		 * In a given state, there can be up to 7 possible moves.  These are:
+		 * 
+		 * 0 1 2
+		 * 3 C 4
+		 * 5 6 7
+		 * 
+		 * We will need to check these 7 possible moves.
+		 */
+		
+		// Move 0 is clear!
+		if((row - 1) >= 0 && (col - 1) >= 0 && !copyMap.get(row-1).get(col - 1).equalsIgnoreCase("X")) {
+			Vector<Vector<String>> temp = makeMove(copyMap, row, col, 0);
+			
+			if(!areBoardsSame(temp, currentState.getPrevious())) {
+				// These boards are different!
+				System.out.println("Move 0 is unique!");
+				moves.addElement(temp);
+			} // End if
+		} // End if
+		
+		//System.out.println("After ")
+		
+		// Move 1 is clear!
+		if((row - 1) >= 0 && !copyMap.get(row-1).get(col).equalsIgnoreCase("X")) {
+			Vector<Vector<String>> temp = makeMove(copyMap, row, col, 1);
+		
+			if(!areBoardsSame(temp, currentState.getPrevious())) {
+				// These boards are different!
+				System.out.println("Move 1 is unique!");
+				moves.addElement(temp);
+			} // End if
+		} // End if
+		
+		// Move 2 is clear!
+		if((row - 1) >= 0 && (col + 1) < copyMap.get(0).size() && !copyMap.get(row-1).get(col + 1).equalsIgnoreCase("X")) {
+			Vector<Vector<String>> temp = makeMove(copyMap, row, col, 2);
+			
+			if(!areBoardsSame(temp, currentState.getPrevious())) {
+				// These boards are different!
+				System.out.println("Move 2 is unique!");
+				moves.addElement(temp);
+			} // End if
+		} // End if
+		
+		// Move 3 is clear!
+		if((col - 1) >= 0 && !copyMap.get(row).get(col - 1).equalsIgnoreCase("X")) {
+			Vector<Vector<String>> temp = makeMove(copyMap, row, col, 3);
+				
+			if(!areBoardsSame(temp, currentState.getPrevious())) {
+				// These boards are different!
+				System.out.println("Move 3 is unique!");
+				moves.addElement(temp);
+			} // End if
+		} // End if
+		
+		// Move 4 is clear!
+		if((col + 1) < copyMap.get(0).size() && !copyMap.get(row).get(col + 1).equalsIgnoreCase("X")) {
+			Vector<Vector<String>> temp = makeMove(copyMap, row, col, 4);
+						
+			if(!areBoardsSame(temp, currentState.getPrevious())) {
+				// These boards are different!
+				System.out.println("Move 4 is unique!");
+				moves.addElement(temp);
+			} // End if
+		} // End if
+				
+		// Move 5 is clear!
+		if((row + 1) < copyMap.size() && (col - 1) >= 0 && !copyMap.get(row+1).get(col - 1).equalsIgnoreCase("X")) {
+			Vector<Vector<String>> temp = makeMove(copyMap, row, col, 5);
+						
+			if(!areBoardsSame(temp, currentState.getPrevious())) {
+				// These boards are different!
+				System.out.println("Move 5 is unique!");
+				moves.addElement(temp);
+			} // End if
+		} // End if
+				
+		// Move 6 is clear!
+		if((row + 1) < copyMap.size() && !copyMap.get(row+1).get(col).equalsIgnoreCase("X")) {
+			Vector<Vector<String>> temp = makeMove(copyMap, row, col, 6);
+					
+			if(!areBoardsSame(temp, currentState.getPrevious())) {
+				// These boards are different!
+				System.out.println("Move 6 is unique!");
+				moves.addElement(temp);
+			} // End if
+		} // End if
+				
+		// Move 7 is clear!
+		if((row + 1) < copyMap.size() && (col + 1) < copyMap.get(0).size()
+				&& !copyMap.get(row+1).get(col+1).equalsIgnoreCase("X")) {
+			Vector<Vector<String>> temp = makeMove(copyMap, row, col, 7);
+					
+			if(!areBoardsSame(temp, currentState.getPrevious())) {
+				// These boards are different!
+				System.out.println("Move 7 is unique!");
+				moves.addElement(temp);
+			} // End if
+		} // End if
+			
 		return moves;
 	} // End for
 	
 	public void AStarAlgorithm() {
 		
 		stepsToHere.addElement(startBoard);
+		
+		Vector<Vector<String>> emptyBoard = new Vector<Vector<String>>();
+		for(int i = 0; i < startBoard.size(); i++) {
+			Vector<String> nextRow = new Vector<String>();
+			
+			for(int j = 0; j < startBoard.get(i).size(); j++) {
+				nextRow.addElement("0");
+			} // End for
+			emptyBoard.addElement(nextRow);
+		} // End for
+		
 		// Create the initial state:
-		State initialState = new State(startBoard, 0, null, stepsToHere);
+		State initialState = new State(startBoard, 0, emptyBoard, stepsToHere);
 		pq.add(initialState);
 		
 		// Begin the A* Algorithm:  
@@ -202,9 +422,30 @@ public class AStar {
 					printBoard(nextState.getStepsToState().get(i));
 				}
 				
+				System.out.println("Size of getStepsToState: " + nextState.getStepsToState().size());
+				
 				System.out.println("Number of steps from start to finish: " + nextState.getNumMoves());
 				break;
 			}
+			
+			// Find all possible moves that we can make:
+			Vector<Vector<Vector<String>>> possiblemoves = possibleMoves(nextState);
+			
+			
+			// For each possible move, create and add a new state to the priority queue:
+			for(int i = 0; i < possiblemoves.size(); i++) {
+				
+				Vector<Vector<Vector<String>>> stepsToHere = nextState.getStepsToState();
+				stepsToHere.addElement(possiblemoves.get(i));
+				
+				State newState = new State(possiblemoves.get(i), nextState.getNumMoves() + 1, 
+										   nextState.getCurrentState(), stepsToHere);
+				
+				if(!hasBoardBeenExplored(possiblemoves.get(i), exploredBoards)) {
+					pq.add(newState);
+				}
+			} // End for
+			
 			
 			if(pq.isEmpty()) {
 				System.err.println("No Solution Found!");
